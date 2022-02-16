83b3895fa8f7b6fb2b36d25b232184421f962da4
Gustin Schumacher <97652359+GustinSchumacher@users.noreply.github.com>
2022-02-16 14:35:09 -0600

    updated variables

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import requests
+import time
 from bs4 import BeautifulSoup
 
 
 def extract(page):
     headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:96.0) Gecko/20100101 Firefox/96.0'}
-    url = f'https://www.indeed.com/jobs?q=web+developer&l=Saint+Paul%2C+MN&start=0'
+    url = f'https://www.indeed.com/jobs?q=Web+Developer&l=Saint+Paul%2C+MN&radius=100&fromage=1'
     r = requests.get(url, headers)
     soup = BeautifulSoup(r.content, 'html.parser')
     return soup
@@ -13,20 +14,34 @@ def extract(page):
 
 def transform(soup):
     divs = soup.find_all('div', class_='job_seen_beacon')
+
     for item in divs:
-        title = item.find('h2').text.strip()
+        try:
+            post_date = item.find('span', 'date').text.strip().replace('Posted', '')
+        except AttributeError:
+            post_date = ''
+        title = item.find('h2').text.strip().replace('new', '')
         company = item.find('span', class_='companyName').text.strip()
         try:
             estimatedsalary = item.find('span', class_='estimated-salary').text.strip()
         except:
             estimatedsalary = ''
+        try:
+            salary = item.find('div', class_='metadata salary-snippet-container').text.strip()
+        except:
+            salary = ''
         summary = item.find('div', class_='job-snippet').text.strip().replace('\n', '')
 
         job = {
+            'post date': post_date,
             'title': title,
             'company': company,
-            'estimatedSalary': estimatedsalary,
+            'estimated salary': estimatedsalary,
+            'salary': salary,
             'summary': summary,
+
+
+
         }
         joblist.append(job)
     return
@@ -34,7 +49,7 @@ def transform(soup):
 
 joblist = []
 
-for i in range(0, 50, 10):
+for i in range(1, 5, 1):
     print(f'Getting page, {i}')
     c = extract(0)
     transform(c)
@@ -43,4 +58,7 @@ df = pd.DataFrame(joblist)
 
 print(df.head())
 
-df.to_csv('jobs.csv')
+TodaysDate = time.strftime("%Y-%m-%d")
+excelfilename = 'jobs-' + TodaysDate +".csv"
+
+df.to_csv(excelfilename)
